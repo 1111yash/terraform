2cda33b9870c11923ab0f42f1f01c33098768aba
1111yash <yashwanjari550@gmail.com>
2026-06-03 21:26:53 +0530

    Updated ec2 config

---
+++ ec2.tf
@@ -0,0 +1,19 @@
+provider "aws"  {
+region = "us-east-1"
+access_key = "ygagdh"
+secret_key = "hagdhas"
+}
+resource "aws_instance" "myec2" {
+ami = "ami-0685bcc683dadb6b9"
+instance_type = "t3.micro"
+key_name = "terraform"
+vpc_security_group_ids = [ "sg1"]
+tags = {
+
+  Name = "myec2"
+  name = "yash"
+  env = "dev"
+
+}
+
+}
