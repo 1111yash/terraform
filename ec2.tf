610e45384e32f920c7a268879e40eb8ca4340934
1111yash <yashwanjari550@gmail.com>
2026-06-04 21:05:04 +0530

    first terraform file added

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "myec2" {
 ami = "ami-0685bcc683dadb6b9"
 instance_type = "t3.micro"
 key_name = "terraform"
-vpc_security_group_ids = [ "sg1"]
+vpc_security_group_ids = ["sg-090b2a83d53a2eee6"]
 tags = {
 
   Name = "myec2"
