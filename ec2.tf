0ed8803f036b1ccde8dc531450f0dbec9b65fa75
1111yash <yashwanjari550@gmail.com>
2026-06-09 13:44:25 +0530

    Added Terraform variables and EC2 configuration

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,24 @@
-provider "aws"  {
-region = "us-east-1"
-
+terraform {
+  backend "s3" {
+    bucket = var.mybucket
+    region = var.region
+    key    = "tfstate"
+  }
 }
+
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_instance" "myec2" {
-ami = "ami-0685bcc683dadb6b9"
-instance_type = "t3.micro"
-key_name = "terraform"
-vpc_security_group_ids = ["sg-090b2a83d53a2eee6"]
-tags = {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = var.mysg
 
-  Name = "myec2"
-  name = "yash"
-  env = "dev"
-
-}
-
+  tags = {
+    Name = var.instance-name
+    name = "suraj"
+    env  = "dev"
+  }
 }
